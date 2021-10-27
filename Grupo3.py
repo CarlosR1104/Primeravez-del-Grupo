a3a9e5954e25eebb5f6027011ada46f58cba2f0c
Jaider Suaza <jaidersuazag@gmail.com>
2021-10-27 12:00:00 -0500

    Vamos a agregar la division

--- Grupo3.py
+++ Grupo3.py
@@ -50,3 +50,21 @@
 
 
 
+
+
+
+
+
+
+
+def IngresarNumero():
+
+numero1=int(input("ingrese el primer  numero:"))
+
+numero2=int(input("ingrese el segundo numero:"))
+
+if numero2==0:
+    print("el divisor no puede ser cero")
+
+else:
+    print(numero1/numero2)
